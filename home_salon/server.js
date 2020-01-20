3ab40cdcef0c3211f554140c75df64d8eb882e54
Jye Bussa <bussajye@gmail.com>
2020-01-20 20:59:18 +1000

    server

--- home_salon/server.js
+++ home_salon/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 // call express and store in app variable
 const app = express()
 
-// Get req to '/' to test server
-app.get('/', (req, res) => res.send('Home Page'))
+// Get req to '/' to make sure everythings working properly
+app.get('/', (req, res) => res.send('API running correctly'))
 
 // Create a port variable (variable when deployed, otherwise 5000)
 const PORT = process.env.PORT || 5000
