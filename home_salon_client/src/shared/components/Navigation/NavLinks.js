b0080aca76791030554a5ea64346043583eaee13
SaraCah <saracaveg@gmail.com>
2020-01-22 13:58:50 +1000

    just added the extra links for the navbar

--- home_salon_client/src/shared/components/Navigation/NavLinks.js
+++ home_salon_client/src/shared/components/Navigation/NavLinks.js
@@ -4,14 +4,24 @@ import { NavLink } from 'react-router-dom';
 import './NavLinks.css';
 
 const NavLinks = props => {
-  return <ul className="nav-links">
-    <li>
-      <NavLink to="/" exact>HOME</NavLink>
-    </li>
-    <li>
-      <NavLink to="/u1/places">Packages</NavLink>
-    </li>
-  </ul>
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Packages</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Testimonials</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Contact</NavLink>
+      </li>
+    </ul>
+  );
 };
 
 export default NavLinks;
