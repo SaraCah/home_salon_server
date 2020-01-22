95884c8dd989bfb60e4934c4bc1f5e078d8d38c8
SaraCah <saracaveg@gmail.com>
2020-01-22 12:36:38 +1000

    routes and package

--- home_salon_client/src/App.js
+++ home_salon_client/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
 
 function App() {
   return (
