e2c38bb3f0f1235713467bb726b7a4dd8765c6fd
Jye Bussa <bussajye@gmail.com>
2020-01-22 16:27:18 +1000

    file struct

--- home_salon_client/src/App.js
+++ home_salon_client/src/App.js
@@ -1,30 +1,25 @@
-
-import React from 'react';
-import './App.css';
+import React from 'react'
+// import './App.css'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from "react-router-dom";
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-
-
-import React from 'react'
+} from 'react-router-dom'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 function App() {
   return (
     <Router>
-      < MainNavigation />
+      <MainNavigation />
       <main>
         <Switch>
-          <Route path="/" exact>
-          </Route>
-          <Redirect to="/" />
+          <Route path='/' exact></Route>
+          <Redirect to='/' />
         </Switch>
       </main>
     </Router>
-  );
+  )
 }
 
 export default App
