aa35259e8949bcc986b51029cd18660ec359f53c
Jye Bussa <bussajye@gmail.com>
2020-01-23 16:12:41 +1000

    nav

--- home_salon_client/src/Components/Navigation/Header.js
+++ home_salon_client/src/Components/Navigation/Header.js
@@ -1,47 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core'
-import { Tabs, Toolbar } from '@material-ui/core'
-import { Typography } from '@material-ui/core'
-import { AppBar } from '@material-ui/core'
+import { Button, IconButton, AppBar, Toolbar } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
-import { Button } from '@material-ui/core'
 const Header = () => {
-  const navStyles = makeStyles({
+  const navStyles = makeStyles(theme => ({
     root: {
       backgroundColor: '#ffa0ea5b',
-      justifyContent: 'flex-end'
+      justifyContent: 'flex-end',
+      // display: 'block',
+      '@media (max-width: 800px)': {
+        justifyContent: 'flex-start'
+        // display: 'none'
+      }
+    },
+    menuButton: {
+      display: 'none',
+      '@media (max-width: 800px)': {
+        position: 'relative',
+        marginRight: '60vw',
+        display: 'block'
+      }
     }
-  })
+  }))
+  const navList = ['Home', 'About', 'Packages', 'Testimonials', 'Contact']
   const classes = navStyles()
+  let navItem = navList.map(item => (
+    <Button>
+      {item === 'Home' ? (
+        <Link to='/' exact>
+          {item}
+        </Link>
+      ) : (
+        <Link to={`/${item}`} exact>
+          {item}
+        </Link>
+      )}
+    </Button>
+  ))
+
   return (
     <AppBar position='static' classes={{ root: classes.root }}>
       <Toolbar classes={{ root: classes.root }}>
-        <Button>
-          <Link to='/' exact>
-            Home
-          </Link>
-        </Button>
-        <Button>
-          <Link to='/about' exact>
-            About
-          </Link>
-        </Button>
-        <Button>
-          <Link to='/packages' exact>
-            Packages
-          </Link>
-        </Button>
-        <Button>
-          <Link to='/testimonials' exact>
-            Testimonials
-          </Link>
-        </Button>
-        <Button>
-          <Link to='/contact' exact>
-            Contact
-          </Link>
-        </Button>
+        <IconButton
+          edge='start'
+          className={classes.menuButton}
+          color='inherit'
+          aria-label='menu'
+        >
+          <MenuIcon />
+        </IconButton>
+        {navItem}
       </Toolbar>
     </AppBar>
   )
