5a8d0e11562d7b1d77d3210df29d83e1710bd518
Jye Bussa <bussajye@gmail.com>
2020-01-30 23:01:06 +1000

    contact

--- home_salon_client/src/Components/pages/Contact.js
+++ home_salon_client/src/Components/pages/Contact.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Icon } from '@material-ui/core'
+import PhoneIcon from '@material-ui/icons/Phone'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(10)
+  },
+  paper: {
+    height: 400,
+    width: 400
+  }
+}))
 
 const Contact = () => {
+  const classes = useStyles()
   return (
     <div>
+      <Icon>
+        <PhoneIcon />
+        <h4>Phone Number:</h4> 0404579007
+      </Icon>
+
       <h1>Opening Hours</h1>
       <h3>Wednesday to Friday</h3>
       <h4>5:45 am - 1:30 pm</h4>
