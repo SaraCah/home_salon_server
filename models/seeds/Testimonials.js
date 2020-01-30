12cd3f6322ef16e8833d7bdb36850f8b8de048f3
Jye Bussa <bussajye@gmail.com>
2020-01-30 18:43:24 +1000

    heroku fixed -.-

---
+++ models/seeds/Testimonials.js
@@ -0,0 +1,8 @@
+// const TestimonialModel = require('../testimonials')
+
+// const Testimonial = TestimonialModel.create({
+//   name: 'Julie-anne Baxter',
+//   date: '',
+//   comment:
+//     'Visited for the first time today. She did a fantastic job with my blonde foils and she is so reasonably priced! Great atmosphere and very friendly.\n Beautifully clean salon! Good on you for making hair care so affordable. Will recommend to all my friends!'
+// })
