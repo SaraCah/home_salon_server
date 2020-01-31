360c3b3182c235b80d4aa4c344f294f28bffc3d2
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-01-31 23:27:26 +1000

    finally figured out how to use grid

--- home_salon_client/src/Components/pages/Home.js
+++ home_salon_client/src/Components/pages/Home.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import CheckIcon from '@material-ui/icons/Check';
-
 import './../../stylesheets/Home.scss'
 
 class Home extends Component {
@@ -10,12 +9,17 @@ class Home extends Component {
   render() {
     return (
       <div className="landing">
-           <h1 className="section-intro1__title">
+      <section className="section section-intro1 section-intro3" id="intro3">
+        <div className="container">
+          <Grid container justify="center">
+            <Grid item md={6}>
+              <h1 className="section-intro1__title">
               Welcome to my Home Salon Redcliffe
               </h1>
               <div className="section-intro1__subtitle">
                 I offer great services such as;
               </div>
+
               <div className="section-intro1__list">
                 <div className="section-intro1__list__item text-muted">
                 < CheckIcon /> Balayage
@@ -34,9 +38,22 @@ class Home extends Component {
                   className="btn-action m-8"
                 >See More?
                 </Fab>
+            </Grid>
+            <Grid item md={6}>
+              <div className="intro3__product">
+                <img
+                  src="https://i.imgur.com/6haFEAO.png"
+                  alt=""
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </section>
       </div>
     );
   }
 }
+
 
 export default Home
