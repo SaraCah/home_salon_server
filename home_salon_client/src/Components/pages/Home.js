887063fa45cc88410ac6d47260bb79a51819cfa9
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-01-31 23:20:19 +1000

    importing future stylesheet

--- home_salon_client/src/Components/pages/Home.js
+++ home_salon_client/src/Components/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import CheckIcon from '@material-ui/icons/Check';
+
+import './../../stylesheets/Home.scss'
+
 const Home = () => {
   return <div></div>
 }
