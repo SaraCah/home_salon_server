e6aa920d8f2aa78faef955966ac636f66115bece
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-01-31 23:20:00 +1000

    chucked in my beautiful checkicon

--- home_salon_client/src/Components/pages/Home.js
+++ home_salon_client/src/Components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
+import CheckIcon from '@material-ui/icons/Check';
 const Home = () => {
   return <div></div>
 }
