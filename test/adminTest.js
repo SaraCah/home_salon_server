e9de6cfd231707d6f0b9f5339876698946049a7a
PhilHarps <thephilipharper@gmail.com>
2020-02-04 13:13:25 +1000

    Fixed adminTest

--- test/adminTest.js
+++ test/adminTest.js
@@ -1,11 +1,27 @@
 const chai = require("chai");
 // chai.use(require('chai-json-schema'));
-const assert = require("chai").assert;
-const chaiHttp = ("chai-http");
+// const assert = require("chai").assert;
+const jwt = require('jsonwebtoken')
+const chaiHttp = require("chai-http");
 const app = require("../server")
+const expect = chai.expect;
 
-describe("AdminModel", function(){
-    it("adminModel should have a user", function(){
-        assert.equal(adminModel(), "user");
-    });
-});
+chai.use(chaiHttp);
+
+describe("Admin Routes", () => {
+    it('Creates an admin user', done => {
+        chai
+            .request(app)
+            .get('/api/admin')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                // decode the JWT token in res
+                // let jwt = jwt.decode()
+                // console.log(jwt)
+                // // expect payload to contain key 'admin'
+                // expect(res).to.contain.keys('admin')
+                done()
+             });        
+    })
+})
