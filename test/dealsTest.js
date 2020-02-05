7e32160e21c55b82d8d86d74a4860089846dd3e7
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-02-05 21:19:09 +1000

    merge conflicts

--- test/dealsTest.js
+++ test/dealsTest.js
@@ -5,32 +5,63 @@ const jwt = require("jsonwebtoken");
 const chaiHttp = require("chai-http");
 const app = require("../server");
 const expect = chai.expect;
-const {DealModel} = require('../models/deals')
-
+const { DealModel } = require("../models/deal");
 
 chai.use(chaiHttp);
 
-describe("Deals Routes", () => {
-    it("Number of Deals recieved matches deals in database", done => {
-        chai
+describe("Packages Routes", () => {
+  it("Number of Packages recieved matches packages in database", done => {
+    chai
+      .request(app)
+      .get("/api/package")
+      .end(async function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        // Get packages from db and expect the length to be the same as the packages returned in res
+        const count = await DealModel.countDocuments();
+        expect(res.body.length).to.equal(count);
+        done();
+      });
+  });
+  it("Should Update a package by package id", done => {
+    let package = new DealModel({
+      packageName: "Test Package 2",
+      price: 220,
+      services: ["Baliage", "Toner"]
+    });
+    package.save((err, deal) => {
+      // Login
+      chai
         .request(app)
-        .get("/api/deal")
-        .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            // Get deals from db and expect the length to be the same as the deals returned in res
-            expect(res.body.length).to.be.equal(1)
-            done()
+        .post("/api/login")
+        .send({
+          name: "Maxine",
+          password: "philharper1"
+        })
+        .end((err, res) => {
+          // Logged in successfully
+          const token = res.body.token;
+
+          // Request package update
+          chai
+            .request(app)
+            .patch("/api/package/" + package._id)
+            .set("x-auth-token", token)
+            .send({
+              //   packageName: "Test Package 2",
+              price: 200
+              //   services: ["Baliage", "Toner"]
+            })
+            .end((err, res) => {
+                // console.log(res.body)
+              expect(res).to.have.status(200);
+              expect(res.body).to.be.a("object");
+              expect(res.body)
+                .to.have.property("price")
+                .eq(200);
+              done();
+            });
         });
     });
-    it("Update a deal", done => {
-        chai
-        .request(app)
-        .get("/api/deal")
-        .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            done()
-        });
-    });
+  });
 });
