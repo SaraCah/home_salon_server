7ce7982aec345063292c09adcdfcf86b6f8a9a8f
Jye Bussa <bussajye@gmail.com>
2020-02-05 22:23:14 +1000

    api connect fixes

--- home_salon_client/src/actions/deal.js
+++ home_salon_client/src/actions/deal.js
@@ -1,75 +1,68 @@
-import axios from 'axios';
+import axios from 'axios'
 import { setAlert } from './alert'
-import {
-    GET_DEALS,
-    DEAL_ERROR,
-    DELETE_DEAL,
-    ADD_DEAL
-} from './types'
-
+import { GET_DEALS, DEAL_ERROR, DELETE_DEAL, ADD_DEAL } from './types'
 
 // GET DEALS
 
 export const getDeals = () => async dispatch => {
-    try {
-        const res = await axios.get('http://localhost:5000/api/deal');
+  try {
+    const res = await axios.get('/api/deal')
 
-        dispatch({
-            type: GET_DEALS,
-            payload: res.data
-        })
-
-    } catch (err) {
-        dispatch({
-            type: DEAL_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status }
-        });
-    }
-    
+    dispatch({
+      type: GET_DEALS,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: DEAL_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
 }
 
 // Delete post
 export const deleteDeal = id => async dispatch => {
-    try {
-      await axios.delete(`http://localhost:5000/api/deal/${id}`);
-  
-      dispatch({
-        type: DELETE_DEAL,
-        payload: id
-      });
-  
-      dispatch(setAlert('Deal Removed'));
-    } catch (err) {
-      dispatch({
-        type: DEAL_ERROR,
-        payload: { msg: err.response.statusText, status: err.response.status }
-      });
-    }
-  };
+  try {
+    await axios.delete(`/api/deal/${id}`)
 
+    dispatch({
+      type: DELETE_DEAL,
+      payload: id
+    })
 
+    dispatch(setAlert('Deal Removed'))
+  } catch (err) {
+    dispatch({
+      type: DEAL_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
 
 // Add Deal
 export const addDeal = formData => async dispatch => {
-
   const config = {
     headers: {
       'Content-Type': 'application/json'
     }
   }
   try {
-    const res = await axios.post(`http://localhost:5000/api/deal/`, formData, config);
+    const res = await axios.post(
+      `http://localhost:5000/api/deal/`,
+      formData,
+      config
+    )
 
     dispatch({
       type: ADD_DEAL,
       payload: res.data
-    });
+    })
 
-    dispatch(setAlert('Post Created'));
+    dispatch(setAlert('Post Created'))
   } catch (err) {
     dispatch({
       type: DEAL_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
-    });
+    })
   }
-};
+}
