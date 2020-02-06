9e1b95d0caaf0a297646e146edfcc64095433fe5
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-02-06 18:47:47 +1000

    fixed footer

--- home_salon_client/src/Components/Navigation/Footer.js
+++ home_salon_client/src/Components/Navigation/Footer.js
@@ -29,12 +29,6 @@ const Footer = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
         <Link to='/login'>Login</Link>
       </li>
     </ul>
