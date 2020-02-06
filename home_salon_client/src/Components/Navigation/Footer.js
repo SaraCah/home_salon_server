52eee1cc54f5ee7056b7a7bc9c94f064b0e253b3
sara.cave1 <sara.cave@gaiaresources.com.au>
2020-02-06 21:37:13 +1000

    lol

--- home_salon_client/src/Components/Navigation/Footer.js
+++ home_salon_client/src/Components/Navigation/Footer.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { logout } from '../../actions/auth'
 import { IconButton, AppBar, makeStyles } from '@material-ui/core'
-=======
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { logout } from '../../actions/auth';
-import { IconButton, AppBar, makeStyles, BottomNavigation } from '@material-ui/core'
->>>>>>> cb44103bc63dac938542a3c5c380956dd525772e
 
 const Footer = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
